a8b53d7c76a6f10903ac71ac0c4f2ab910020faa
doormatt-dev <mattkeeble@web.de>
2021-07-30 18:24:19 +0200

    user prefs manager and persistence, still buggy, needs settings UI

---
+++ Assets/Scripts/UserPrefMgr.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPrefMgr : MonoBehaviour
+{
+    public static UserPrefMgr settings;
+
+    public int graphicsLevel;
+    public int saveSlotNr;
+    public bool showOnScreenControls;
+
+    private GameObject OSC;
+
+    void Awake()
+    {
+        //as soon as this Object exists check if there is already one and then destroy self or become the only one
+        if(settings == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            settings = this;
+            Debug.Log("New UserPrefMgr set");
+        }else if(settings != this){
+            Debug.Log("UserPrefMgr duplicate destroyed");
+            Destroy(gameObject);
+        }
+
+        //Obtain User Prefs
+        graphicsLevel = PlayerPrefs.GetInt("graphicsLevel", 2);
+        saveSlotNr = PlayerPrefs.GetInt("saveSlot",0);
+        showOnScreenControls = (PlayerPrefs.GetInt("showOSC",1) != 0);
+
+        //Set quality level accordingly
+        if(UnityEngine.QualitySettings.GetQualityLevel() != graphicsLevel)
+        {
+            UnityEngine.QualitySettings.SetQualityLevel(graphicsLevel);
+            Debug.Log("Graphics quality set to "+ graphicsLevel);
+        }else{
+            Debug.Log("Graphics quality already set to " + graphicsLevel);
+        }
+
+        Debug.Log("Save slot is " + saveSlotNr);
+
+        //Show or hide the on screen controls if there are any
+        OSC = GameObject.Find("/UI Canvas/Onscreen Controls");
+        if(OSC != null)
+        {
+            OSC.SetActive(showOnScreenControls);
+            Debug.Log("onscreen controls set to " + showOnScreenControls);
+        }else{
+            Debug.Log("No osc present");
+        }
+        
+
+
+    }
+    
+}
---
+++ Assets/Scripts/loadHomeMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadHomeMenu : MonoBehaviour
+{
+    public void loadHomeMenuScene()
+    {
+        Debug.Log("loading scene 0");
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}

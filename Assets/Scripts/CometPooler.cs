1a71f015b78aedda09fa86ca01ca300a628e7c7d
doormatt-dev <mattkeeble@web.de>
2021-10-01 16:23:04 +0200

    comet spawning system automatic now

--- Assets/Scripts/CometPooler.cs
+++ Assets/Scripts/CometPooler.cs
@@ -9,9 +9,16 @@ public class CometPooler : MonoBehaviour
     [SerializeField] float despawndistance;
     [SerializeField] public float spawndistance;
     [SerializeField] GameObject Player;
+    [SerializeField] bool spawnAutomatically;
+    [SerializeField] float minTimeBetweenSpawns;
+    [SerializeField] float minDistanceMovedBetweenSpawns;
+    [SerializeField] float minDistanceBetweenCometSpawns;
 
     public static CometPooler CometPool;
     public List<GameObject> cometPool;
+    Vector3 previousSpawnPos,previousShipPos;
+    double timeOfLastSpawn;
+    bool farenoughMoved,longEnoughDelay;
 
     void Awake()
     {
@@ -27,6 +34,34 @@ public class CometPooler : MonoBehaviour
             tmp.transform.SetParent(transform);
             tmp.SetActive(false);
             cometPool.Add(tmp);
+        }
+    }
+
+    void Update()
+    {
+        farenoughMoved = Vector3.Distance(Player.transform.position,previousShipPos) > minDistanceMovedBetweenSpawns;//did you move far enough
+        longEnoughDelay = Time.unscaledTime - timeOfLastSpawn >= minTimeBetweenSpawns;// has the min amount of time passed
+
+        if(spawnAutomatically && longEnoughDelay && farenoughMoved)
+        {//only do this if you're supposed to and enough time has passed spawn comets else don't
+
+            GameObject objectToSpawn = GetComet();
+            if(objectToSpawn != null)
+            {
+                //Vector3 spawnpos = Player.transform.position + (Player.GetComponent<Rigidbody>().velocity.normalized * spawndistance);
+                Vector3 spawnpos = previousSpawnPos;
+                while(Vector3.Distance(previousSpawnPos,spawnpos) < minDistanceBetweenCometSpawns)//make sure the comets aren`t piling up
+                {
+                    spawnpos = Player.transform.rotation * (new Vector3(Random.Range(-0.5f,0.5f),Random.Range(-0.5f,0.5f),1).normalized * spawndistance) + Player.transform.position;//position of new comet infront of the ship's direction of travel
+                }
+
+                objectToSpawn.transform.SetPositionAndRotation(spawnpos,Random.rotation);//put it there and roatate to whatever orientation to not make it boring
+                objectToSpawn.SetActive(true);//set it active
+                previousSpawnPos = spawnpos;
+                previousShipPos = Player.transform.position;
+                timeOfLastSpawn = Time.unscaledTime;
+            }
+            
         }
     }
 
